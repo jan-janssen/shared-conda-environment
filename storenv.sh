4c8a7c693f11a263cad3e673a63890ed0f1ce305
Jan Janssen <jan.janssen@outlook.com>
2022-06-20 20:29:18 -0600

    automate using storenv.sh script

---
+++ storenv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+repo_path="/Users/jan/PycharmProjects/my-conda-environment"
+apple_path=${repo_path}"/macmini_apple"
+intel_path=${repo_path}"/macmini_intel"
+
+# store apple environment
+cd ${apple_path}
+source ~/.zshrcapple
+conda update --all -y
+conda env export > environment.yml
+git add environment.yml
+
+# store intel environment
+cd ${intel_path}
+source ~/.zshrcintel
+conda update --all -y
+conda env export > environment.yml
+git add environment.yml
