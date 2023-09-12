0534660d0374b69fea298aaddd1bc59b8cc531e5
Jan Janssen <janssen@mpie.de>
2023-09-12 09:41:03 +0200

    rename linux environment

--- storenv_linux.sh
+++ storenv_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 repo_path="/home/janssen/projects/my-conda-environment"
-env_path=${repo_path}"/linux_workstation"
+env_path=${repo_path}"/linux_x64"
 
 # store intel environment
 cd ${env_path}
