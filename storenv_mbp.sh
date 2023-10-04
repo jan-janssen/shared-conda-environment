a87329c54ac942db2b2bdf235b5117d6fb31b51a
Jan Janßen <janssen@mpie.de>
2023-10-04 07:20:16 +0200

    macbook pro update

--- storenv_mbp.sh
+++ storenv_mbp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo_path="/Users/jan/PycharmProjects/my-conda-environment"
+repo_path="/Users/jan/projects/my-conda-environment"
 intel_path=${repo_path}"/macbookpro_x64"
 
 # store intel environment
