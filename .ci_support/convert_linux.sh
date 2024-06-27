a3961dc7c1a98359cb2f2e1474125550d69d4bd5
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-06-27 12:39:03 +0200

    Update convert_linux.sh

--- .ci_support/convert_linux.sh
+++ .ci_support/convert_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed '/\[win\]/d' environment.yml | sed '/\[osx\]/d' | sed '/\[macos\]/d' > tmp.yml
+sed -e '/\[win\]/d' -e '/\[osx\]/d' -e '/\[macos\]/d' environment.yml  > tmp.yml
 mv tmp.yml environment.yml
