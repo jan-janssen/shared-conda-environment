615bdfd7bb59d49ef9a2b97b79691297fc2046b5
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-27 15:44:20 +0200

    Update convert_windows.sh

--- .ci_support/convert_windows.sh
+++ .ci_support/convert_windows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -e '/\[unix\]/d' -e '/\[linux\]/d' -e '/\[osx\]/d' -e '/\[macos\]/d' environment.yml  > tmp.yml
+sed -e '/\[unix\]/d' -e '/\[linux\]/d' -e '/\[macarm\]/d' -e '/\[macos\]/d' environment.yml  > tmp.yml
 mv tmp.yml environment.yml
