e83acd84d327fb50e3c66797d1c9b83741492b1e
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-27 15:51:10 +0200

    Update convert_macarm.sh

--- .ci_support/convert_macarm.sh
+++ .ci_support/convert_macarm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -e '/\[win\]/d' -e '/\[linux\]/d' -e '/\[macos\]/d' -e '/\[unix64\]/d' environment.yml > tmp.yml
+sed -e '/\[win\]/d' -e '/\[linux\]/d' -e '/\[macos\]/d' -e '/\[unix64\]/d' -e '/\[x64\]/d' environment.yml > tmp.yml
 mv tmp.yml environment.yml
