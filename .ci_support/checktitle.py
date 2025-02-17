4b927e000f2505f63d1d7322cabf985463a9a0fc
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-02-17 11:51:28 +0100

    Update checktitle.py

--- .ci_support/checktitle.py
+++ .ci_support/checktitle.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     pr_title = sys.argv[1]
     with open("disableautomerge.txt", "r") as f:
         disable_lst = f.readlines()
-    for disable in disable_lst:
-        if len(disable.rstrip()) > 0 and disable.rstrip() in pr_title:
-            sys.exit(1)
+    if len(pr_title) > 0:
+        for disable in disable_lst:
+            if len(disable.rstrip()) > 0 and disable.rstrip() in pr_title:
+                sys.exit(1)
